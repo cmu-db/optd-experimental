55549328e5637dadd3bf22e673cf45693bc00f14
Sarvesh Tandon <sarveshtandonus@gmail.com>
2024-10-26 18:08:37 -0400

    first draft of completed schema
    This commit creates the `migrator` and `entities` modules, as well as
    adds a small demo and `migrate` binary to make creating the `sqlite.db`
    migrated file easier.
    
    The tables currently implemented are all of them. The cascade ones are:
    - `cascades_group`
    - `logical_expression`
    - `logical_group_junction`
    - `logical_property`
    - `physical_expression`
    - `physical_group_junction`
    - `physical_property`
    
    Update .gitignore
    
    Added all the tables
    
    Co-Authored-By: Connor Tsui <87130162+connortsui20@users.noreply.github.com>

---
+++ optd-persistent/src/bin/migrate.rs
@@ -0,0 +1,16 @@
+use optd_persistent::{migrate, DATABASE_URL};
+use sea_orm::*;
+use sea_orm_migration::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    let _ = std::fs::remove_file(DATABASE_URL);
+
+    let db = Database::connect(DATABASE_URL)
+        .await
+        .expect("Unable to connect to the database");
+
+    migrate(&db)
+        .await
+        .expect("Something went wrong during migration");
+}
