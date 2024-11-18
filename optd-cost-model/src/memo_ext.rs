e7309254dc249669f5eae4b4efe4eed23e6e92b5
Yuanxin Cao <xx01cyx@gmail.com>
2024-11-17 20:47:49 -0500

    add get_attribute_info interface to MemoExt

--- optd-cost-model/src/memo_ext.rs
+++ optd-cost-model/src/memo_ext.rs
@@ -1,9 +1,10 @@
 use crate::common::{
-    properties::{attr_ref::GroupAttrRefs, schema::Schema},
+    properties::{attr_ref::GroupAttrRefs, schema::Schema, Attribute},
     types::GroupId,
 };
 
 pub trait MemoExt {
-    fn get_schema_of(&self, group_id: GroupId) -> Schema;
-    fn get_column_ref_of(&self, group_id: GroupId) -> GroupAttrRefs;
+    fn get_schema(&self, group_id: GroupId) -> Schema;
+    fn get_column_ref(&self, group_id: GroupId) -> GroupAttrRefs;
+    fn get_attribute_info(&self, group_id: GroupId, attr_ref_idx: u64) -> Attribute;
 }
