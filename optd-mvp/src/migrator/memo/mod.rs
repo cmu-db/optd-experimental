06495a125628101d824891a277ea5558eea3d9a0
Connor Tsui <connor.tsui20@gmail.com>
2024-11-27 11:03:32 -0500

    add migration and entity files

---
+++ optd-mvp/src/migrator/memo/mod.rs
@@ -0,0 +1,14 @@
+//! Entities related to the memo table used for dynamic programming in the Cascades query
+//! optimization framework.
+
+pub(crate) mod m20241127_000001_cascades_group;
+pub(crate) mod m20241127_000001_logical_children;
+pub(crate) mod m20241127_000001_logical_expression;
+pub(crate) mod m20241127_000001_physical_children;
+pub(crate) mod m20241127_000001_physical_expression;
+
+pub(crate) use m20241127_000001_cascades_group as cascades_group;
+pub(crate) use m20241127_000001_logical_children as logical_children;
+pub(crate) use m20241127_000001_logical_expression as logical_expression;
+pub(crate) use m20241127_000001_physical_children as physical_children;
+pub(crate) use m20241127_000001_physical_expression as physical_expression;
---
+++ optd-mvp/src/migrator/mod.rs
@@ -0,0 +1,18 @@
+use sea_orm_migration::prelude::*;
+
+mod memo;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![
+            Box::new(memo::cascades_group::Migration),
+            Box::new(memo::logical_expression::Migration),
+            Box::new(memo::logical_children::Migration),
+            Box::new(memo::physical_expression::Migration),
+            Box::new(memo::physical_children::Migration),
+        ]
+    }
+}
